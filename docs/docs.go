2302aadae492b2b3d1510402d912b95aa8a07042
kaitobq <kaitobq520@gmail.com>
2025-01-16 16:28:45 +0900

    fixx

--- docs/docs.go
+++ docs/docs.go
@@ -50,7 +50,7 @@ const docTemplate = `{
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
 	Host:             "localhost:8080",
-	BasePath:         "api/v1",
+	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Refty API",
 	Description:      "This is the Refty API server.",
